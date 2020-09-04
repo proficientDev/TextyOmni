e1f85b46f59c0458b54fd95efd42b844b5086905
proficientDev <ultraj0330@gmail.com>
2020-09-04 10:20:52 +0000

    Update to bot response message

--- app/services/message_templates/template/email_collect.rb
+++ app/services/message_templates/template/email_collect.rb
@@ -5,7 +5,8 @@ class MessageTemplates::Template::EmailCollect
     ActiveRecord::Base.transaction do
       conversation.messages.create!(ways_to_reach_you_message_params)
       conversation.messages.create!(email_input_box_template_message_params)
-    end
+      conversation.messages.create!(phone_number_input_box_template_message_params)
+  end
   rescue StandardError => e
     Raven.capture_exception(e)
     true
@@ -37,6 +38,27 @@ class MessageTemplates::Template::EmailCollect
       inbox_id: @conversation.inbox_id,
       message_type: :template,
       content_type: :input_email,
+      content: content
+    }
+  end
+  
+  def phone_number_input_box_template_message_params
+    content = I18n.t('conversations.templates.phone_number_input_box_message_body',
+                     account_name: account.name)
+
+    {
+      account_id: @conversation.account_id,
+      inbox_id: @conversation.inbox_id,
+      message_type: :template,
+      content_type: 'form',
+      content_attributes: {
+        "items": [
+          # { "name": 'email', "placeholder": 'Please enter your email', "type": 'email', "label": 'Email' },
+          # { "name": 'text_aread', "placeholder": 'Please enter text', "type": 'text_area', "label": 'Large Text' },
+          { "name": 'name', "placeholder": 'Please enter your name', "type": 'text', "label": 'Name', "default": '' },
+          { "name": 'phone_number', "placeholder": 'Please enter phone number', "type": 'text', "label": 'Phone Number', "default": '' }
+        ]
+      }, 
       content: content
     }
   end
